81d2a79de89981de7b4be2c4c1af45ee46fc39f7
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2023-02-26 15:14:19 +0530

    Jai Ganesh

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import dotenv from 'dotenv';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { useDispatch } from 'react-redux';
+import './App.css';
+
+import theme from './theme';
+import ScrollToTop from './components/reusables/ScrollToTop';
+import Navbar from './components/reusables/Navbar';
+import Footer from './components/reusables/Footer';
+import { getAllMeals } from './store/actions/productActions';
+import useOnline from './hooks/useOnline.js';
+
+import Routes from './routes';
+
+function App() {
+
+  return (
+    // <ThemeProvider theme={theme}>
+      <>
+      <ScrollToTop />
+      <Navbar />
+      <Routes />
+      <Footer />
+      </>
+    // </ThemeProvider>
+  );
+}
+
+export default App;
