8263a6a76ca7c8d861e8f5d967636967b0c7526f
theadityashrivastava <adityashrivastava23sept@gmail.com>
2023-03-01 01:11:55 +0530

    UI Changes of admin login page and all kitchen page

--- server/dist/app.js
+++ server/dist/app.js
@@ -22,7 +22,7 @@ const limiter = (0, express_rate_limit_1.default)({
 app.use((0, cors_1.default)({
     credentials: true,
     // origin: "http://3.6.178.87/", 
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 app.use((0, helmet_1.default)());
